a79b5a64bd1e9803509e806b2f50494db62650c4
Nichole Byrnes <56095224+Pufferfish3000@users.noreply.github.com>
2025-05-30 11:03:05 -0400

    created basic getopt logic

--- bpff/src/main.c
+++ bpff/src/main.c
@@ -1,13 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "bpff.h"
+
+static void DisplayUsage();
+static int GetOptions(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
     int exit_code = EXIT_FAILURE;
 
+    if (GetOptions(argc, argv))
+    {
+        DisplayUsage();
+        goto end;
+    }
+
     exit_code = StartBPFF();
 
+end:
+    return exit_code;
+}
+
+static void DisplayUsage()
+{
+
+    printf("USAGE");
+}
+
+static int GetOptions(int argc, char* argv[])
+{
+    int exit_code = EXIT_SUCCESS;
+    int option = 0;
+
+    if (NULL == argv)
+    {
+        (void)fprintf(stderr, "argc can not be NULL");
+        goto end;
+    }
+
+    while (-1 != (option = getopt(argc, argv, "l:f:a:h")))
+    {
+        switch (option)
+        {
+
+            case 'l':
+                break;
+
+            case 'h':
+                exit_code = EXIT_FAILURE;
+                break;
+
+            case '?':
+                exit_code = EXIT_FAILURE;
+                break;
+        }
+    }
+
+end:
     return exit_code;
 }
