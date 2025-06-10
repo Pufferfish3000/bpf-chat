821a48222098b8cd773d396e6838da8c7dc5fbe0
Nichole Byrnes <56095224+Pufferfish3000@users.noreply.github.com>
2025-06-10 10:26:48 -0400

    changed python naming as the chat is peer to peer

--- bpfc/src/bpfc.c
+++ bpfc/src/bpfc.c
@@ -1,10 +1,65 @@
-
+#include <arpa/inet.h>
+#include <linux/filter.h>
+#include <linux/if_ether.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int StartBPFC()
+static int CreateUDPFilterSocket(uint16_t port);
+
+int StartBPFC(uint16_t port)
 {
+    int exit_code = EXIT_FAILURE;
+    int sock = -1;
+
+    sock = CreateUDPFilterSocket(port);
+
     printf("Starting BPFC\n");
 
-    return EXIT_SUCCESS;
+    return exit_code;
+}
+
+/**
+ * @brief Creates a raw UDP bpf socket that filters for UDP dst port.
+ * 
+ * @param port UDP dst port to filter for.
+ * @return int the file descriptor of the created socket, or -1 on failure.
+ */
+static int CreateUDPFilterSocket(uint16_t port)
+{
+    int sock = -1;
+    const int code_size = 16;
+    const int port_idx_1 = 5;
+    const int port_idx_2 = 13;
+
+    // udp and dst port = port
+    struct sock_filter code[] = {
+        {0x28, 0, 0, 0x0000000c},  {0x15, 0, 4, 0x000086dd}, {0x30, 0, 0, 0x00000014},
+        {0x15, 0, 11, 0x00000011}, {0x28, 0, 0, 0x00000038}, {0x15, 8, 9, 0xffffffff},
+        {0x15, 0, 8, 0x00000800},  {0x30, 0, 0, 0x00000017}, {0x15, 0, 6, 0x00000011},
+        {0x28, 0, 0, 0x00000014},  {0x45, 4, 0, 0x00001fff}, {0xb1, 0, 0, 0x0000000e},
+        {0x48, 0, 0, 0x00000010},  {0x15, 0, 1, 0xffffffff}, {0x6, 0, 0, 0x00040000},
+        {0x6, 0, 0, 0x00000000},
+    };
+
+    code[port_idx_1].k = port;
+    code[port_idx_2].k = port;
+
+    struct sock_fprog bpf = {
+        .len = code_size,
+        .filter = code,
+    };
+    printf("Filtering packets for udp dst port: %u\n", port);
+
+    sock = CreateRawFilterSocket(&bpf);
+    if (-1 == sock)
+    {
+        (void)fprintf(stderr, "Could not create raw udp filter socket");
+    }
+
+    return sock;
 }
---
+++ peer/__main__.py
@@ -0,0 +1,30 @@
+import logging
+from peer.view import PeerView
+from peer.parse import get_command_args
+from peer.peer import Peer
+
+
+def start_peer() -> None:
+    args = get_command_args()
+    if args.debug:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+
+    peer = Peer(args, log_level)
+
+    intro = PeerView.colored_text(
+        "BPF Chat Peer, type a message to send to another peer\n\n\n",
+        "05A8AA",
+    )
+
+    # Print the intro message without logging
+    print(intro)
+    peer.view.print_debug("Starting Peer in debug mode")
+    try:
+        peer.peer_loop()
+    except KeyboardInterrupt:
+        peer.view.print_msg("Goodbye")
+
+if __name__ == "__main__":
+    start_peer()
